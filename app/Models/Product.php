33ccc17b9ec158344f366cc1be97ab1568daa519
larasaprilia060406-dev <larasaprilia060406@gmail.com>
2026-04-28 12:23:05 +0700

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,10 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'status',
+        'stock',
+        'category_id' 
+    ];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
-    use HasFactory;
 }
