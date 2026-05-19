fd184b8bc1e83f5787e2fc371671e57b65b61f7e
larasaprilia060406-dev <larasaprilia060406@gmail.com>
2026-05-19 14:29:57 +0700

    category

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,21 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
+
+// Route::get('/products', [ProductController::class, 'index']);
 Route::get('/insert', [ProductController::class, 'insert']);
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+// Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+// Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
 //Route::get('/products/update/{id}', [ProductController::class, 'update']);//
 Route::get('/products/delete/{id}', [ProductController::class, 'delete']);
-Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+// Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+// Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
