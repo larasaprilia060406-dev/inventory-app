ff8bbeaf9ca525c522e45740ced4ed6bf241761a
larasaprilia060406-dev <larasaprilia060406@gmail.com>
2026-06-02 12:01:05 +0700

    home

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
 Route::resource('categories', CategoryController::class);
 Route::resource('products', ProductController::class);
 
